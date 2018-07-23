28a6da6a3e83faacb7601a28122c5b433f9be719
Hillevi Eklöw <hillevieklow@gmail.com>
2018-07-23 14:49:07 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,12 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+  end
+
   private
 
     def set_student
